4d9cf42c7e896accecf08b7ee27b6e8e57b66162
how <how@howdeiMac.local>
2019-01-22 17:59:53 +0800

    type scripts support

--- custom.el
+++ custom.el
@@ -12,10 +12,10 @@
  '(ohai-personal-taste/window-state (quote normal))
  '(ohai/modules
    (quote
-    (ohai-appearance ohai-fonts ohai-general ohai-dashboard ohai-html ohai-navigation ohai-editing ohai-ac ohai-vue ohai-markdown ohai-javascript ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-helm ohai-flycheck ohai-git ohai-orgmode ohai-scala ohai-php ohai-help ohai-elisp ohai-haskell)))
+    (ohai-appearance ohai-fonts ohai-general ohai-dashboard ohai-html ohai-navigation ohai-editing ohai-ac ohai-vue ohai-markdown ohai-javascript ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-helm ohai-flycheck ohai-git ohai-orgmode ohai-scala ohai-php ohai-help ohai-elisp ohai-haskell ohai-tide)))
  '(package-selected-packages
    (quote
-    (vue-html-mode vue-mode intero highlight-parentheses paredit discover-my-major which-key php-mode sbt-mode scala-mode org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit helm-flycheck flycheck-color-mode-line flycheck helm-ag helm-descbinds helm-projectile helm projectile dired+ ethan-wspace tern js2-refactor js2-mode json-mode markdown-mode add-node-modules-path auto-complete anzu ace-window avy rainbow-mode web-mode volatile-highlights shrink-whitespace expand-region multiple-cursors dashboard doom-themes diminish spaceline window-numbering hlinum rainbow-delimiters exec-path-from-shell f use-package paradox)))
+    (tide vue-html-mode vue-mode intero highlight-parentheses paredit discover-my-major which-key php-mode sbt-mode scala-mode org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit helm-flycheck flycheck-color-mode-line flycheck helm-ag helm-descbinds helm-projectile helm projectile dired+ ethan-wspace tern js2-refactor js2-mode json-mode markdown-mode add-node-modules-path auto-complete anzu ace-window avy rainbow-mode web-mode volatile-highlights shrink-whitespace expand-region multiple-cursors dashboard doom-themes diminish spaceline window-numbering hlinum rainbow-delimiters exec-path-from-shell f use-package paradox)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (setq-default tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
@@ -25,4 +25,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:height 180 :width normal :family "Courier New" :size 16)))))
